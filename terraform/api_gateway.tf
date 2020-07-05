e80361540ed0802cef016824017c9cdd37383ca4
Björn Petersen <pheasn@gmail.com>
2020-07-05 13:42:52 +0200

    This is working

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -48,6 +48,20 @@ resource "aws_api_gateway_rest_api" "api" {
   }
 }
 
+resource "aws_lambda_permission" "invoke_post" {
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.post.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+resource "aws_lambda_permission" "invoke_get" {
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.get_result.function_name
+    principal = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name = "${var.cloudflare_infix}.${var.cloudflare_zone_name}"
   validation_method = "DNS"
@@ -91,6 +105,81 @@ resource "aws_api_gateway_request_validator" "validate_body" {
   validate_request_body       = true
 }
 
+resource "aws_api_gateway_method" "post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = "POST"
+  authorization = "NONE"
+  request_validator_id = aws_api_gateway_request_validator.validate_body.id
+
+  request_models = {
+    "application/json" = aws_api_gateway_model.initial_request.name
+  }
+}
+
+resource "aws_api_gateway_method_response" "post_ok" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = aws_api_gateway_model.domain_response.name
+  }
+}
+
+resource "aws_api_gateway_method_response" "post_error" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = "400"
+
+  response_models = {
+    "application/json" = "Error"
+  }
+}
+
+resource "aws_api_gateway_integration" "post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = aws_lambda_function.post.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "post_ok_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = aws_api_gateway_method_response.post_ok.status_code
+  selection_pattern = "-"
+
+  depends_on = [
+    aws_api_gateway_integration.post
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "post_error_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = aws_api_gateway_method_response.post_error.status_code
+  selection_pattern = "Invalid.*"
+
+  response_templates = {
+    "application/json" = <<EOF
+{
+  "message" : "$input.path('errorMessage')"
+}
+EOF
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.post
+  ]
+}
+
 resource "aws_api_gateway_method" "get" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_rest_api.api.root_resource_id
@@ -103,6 +192,17 @@ resource "aws_api_gateway_method" "get" {
   }
 }
 
+resource "aws_api_gateway_method_response" "get_ok" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = aws_api_gateway_model.result_response.name
+  }
+}
+
 resource "aws_api_gateway_integration" "get" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_rest_api.api.root_resource_id
@@ -112,6 +212,18 @@ resource "aws_api_gateway_integration" "get" {
   uri = aws_lambda_function.get_result.invoke_arn
 }
 
+resource "aws_api_gateway_integration_response" "result_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.get.http_method
+  status_code = aws_api_gateway_method_response.get_ok.status_code
+
+  depends_on = [
+    aws_api_gateway_integration.get,
+    aws_api_gateway_integration.post
+  ]
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name = "prod"
@@ -119,6 +231,7 @@ resource "aws_api_gateway_deployment" "deployment" {
   triggers = {
     redeployment = sha1(join(",", list(
       jsonencode(aws_api_gateway_integration.get),
+      jsonencode(aws_api_gateway_integration.post)
     )))
   }
 
@@ -127,7 +240,8 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 
   depends_on = [
-    aws_api_gateway_integration.get
+    aws_api_gateway_integration.get,
+    aws_api_gateway_integration.post
   ]
 }
 
@@ -136,11 +250,3 @@ resource "aws_api_gateway_base_path_mapping" "api_domain" {
   domain_name = aws_api_gateway_domain_name.domain_name.domain_name
   stage_name = aws_api_gateway_deployment.deployment.stage_name
 }
-
-# resource "aws_apigatewayv2_integration" "post" {
-#   api_id = aws_apigatewayv2_api.api.id
-#   integration_type = "AWS"
-#   content_handling_strategy = "CONVERT_TO_TEXT"
-#   integration_method = "POST"
-#   integration_uri = aws_lambda_function.post.invoke_arn
-# }
