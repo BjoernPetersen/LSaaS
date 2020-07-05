334e6ef86fcf2b7888c1669d08333e423a9578fc
Björn Petersen <pheasn@gmail.com>
2020-07-05 15:10:48 +0200

    Done

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,8 +77,13 @@ resource "aws_s3_bucket_public_access_block" "private_bucket" {
 
 data "aws_iam_policy_document" "lambda_role_s3_policy" {
   statement {
-    actions = ["s3:PutObject", "s3:GetObject", "s3:DeleteObject", "s3:ListBucket"]
-    resources = [ aws_s3_bucket.bucket.arn ]
+    actions = ["s3:PutObject", "s3:GetObject", "s3:DeleteObject"]
+    resources = [ "${aws_s3_bucket.bucket.arn}/*" ]
+  }
+
+  statement {
+    actions = [ "s3:ListBucket" ]
+    resources = [ aws_s3_bucket.bucket.arn, "${aws_s3_bucket.bucket.arn}/*" ]
   }
 }
 
