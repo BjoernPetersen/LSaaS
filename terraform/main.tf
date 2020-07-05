91ff192b07fa6563f777da1488fcc660ec2a1269
Björn Petersen <pheasn@gmail.com>
2020-07-05 14:09:20 +0200

    Configure bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,27 @@ resource "aws_iam_role" "lambda_role" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.s3_bucket_name
+  acl = "private"
+
+  lifecycle_rule {
+    id = "DeleteOldObjects"
+    enabled = true
+    abort_incomplete_multipart_upload_days = 7
+    expiration {
+      days = 1
+    }
+    noncurrent_version_expiration {
+      days = 1
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private_bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 data "aws_iam_policy_document" "lambda_role_s3_policy" {
