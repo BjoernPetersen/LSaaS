353be170ccdfcd60f3663e96281f4a836d9d003f
Björn Petersen <pheasn@gmail.com>
2020-11-10 11:55:13 +0100

    Use Gradle Java Toolchain feature

--- jks/build.gradle.kts
+++ jks/build.gradle.kts
@@ -8,8 +8,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 repositories {
